b705f5b0dbe456145ab8e0daa9e10fcf741cbe4d
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-07-19 17:41:09 -0500

    update version

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:f_template_juego_taller1/random_words.dart';
 import 'package:flutter/material.dart';
+import 'package:loggy/loggy.dart';
 
 void main() {
-  runApp(MaterialApp(
+  Loggy.initLoggy(
+    logPrinter: const PrettyPrinter(
+      showColors: true,
+    ),
+  );
+  runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "RandomWords",
       home: RandomWords()));
--- lib/random_words.dart
+++ lib/random_words.dart
@@ -1,8 +1,11 @@
 import 'dart:math';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:loggy/loggy.dart';
 
 class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
@@ -10,7 +13,7 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   String _theState = "0";
   int _actualWordType = 0;
-  final _random = new Random();
+  final _random = Random();
 
   int next(int min, int max) => min + _random.nextInt(max - min);
 
@@ -24,10 +27,10 @@ class _RandomWordsState extends State<RandomWords> {
     var option = next(0, 2);
     var randomItem = "";
     if (option == 0) {
-      print("change to noun");
+      logInfo("change to noun");
       randomItem = (nouns.toList()..shuffle()).first;
     } else {
-      print("change to adjective");
+      logInfo("change to adjective");
       randomItem = (adjectives.toList()..shuffle()).first;
     }
   }
@@ -40,22 +43,23 @@ class _RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Random Words"),
+        title: const Text("Random Words"),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Text("$_theState"),
+          Text(_theState),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               ElevatedButton(
-                  onPressed: () => _onPressed(), child: Text("Noun")),
+                  onPressed: () => _onPressed(), child: const Text("Noun")),
               ElevatedButton(
-                  onPressed: () => _onPressed(), child: Text("Adjective"))
+                  onPressed: () => _onPressed(), child: const Text("Adjective"))
             ],
           ),
-          ElevatedButton(onPressed: () => _onReset(), child: Text("Reset")),
+          ElevatedButton(
+              onPressed: () => _onReset(), child: const Text("Reset")),
         ],
       ),
     );
